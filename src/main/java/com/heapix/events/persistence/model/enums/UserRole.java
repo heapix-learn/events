38182dc9ae2b0930cf78536443a5658cd1226d87
Vladislav Yaroshevich 550504 <unclesamserious2012@gmail.com>
2018-05-04 19:13:48 +0300

    added superadmin role and initialization of the database
    (cherry picked from commit 39ae801)

--- src/main/java/com/heapix/events/persistence/model/enums/UserRole.java
+++ src/main/java/com/heapix/events/persistence/model/enums/UserRole.java
@@ -5,10 +5,11 @@ package com.heapix.events.persistence.model.enums;
  */
 public enum UserRole {
 
-    SYSTEM_ADMIN_USER(1L,"Administrator","Can manage any section of a club and modify their content"),
-    MODERATOR_USER(2L,"Moderator","Will be able to access any section of a club, but as observers"),
-    MEMBER_USER(3L,"Member","Will have limited access to the sections related to himself, as well as to common spaces"),
-    ANONYMOUS_USER(4L,"Anonymous","Unregistered external user that can access the public sections of the web page");
+    SYSTEM_SUPER_ADMIN(1L, "Super Administrator", "Can manage any section of a club, modify content and modify permissions of any user"),
+    SYSTEM_ADMIN_USER(2L,"Administrator","Can manage any section of a club and modify their content"),
+    MODERATOR_USER(3L,"Moderator","Will be able to access any section of a club, but as observers"),
+    MEMBER_USER(4L,"Member","Will have limited access to the sections related to himself, as well as to common spaces"),
+    ANONYMOUS_USER(5L,"Anonymous","Unregistered external user that can access the public sections of the web page");
 
     private final Long id;
     private final String role;
